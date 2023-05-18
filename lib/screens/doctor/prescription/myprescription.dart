a215d725d3480daedbf8747f6477fbd8d4147882
Amanuel Demelash <amanueldemelash12@gmail.com>
2023-05-18 11:53:16 -0700

    write priscription

--- lib/screens/doctor/prescription/myprescription.dart
+++ lib/screens/doctor/prescription/myprescription.dart
@@ -19,6 +19,7 @@ class Myprescription extends StatelessWidget {
         ),
         leading: IconButton(
             onPressed: () {
+              Get.back();
             },
             icon: const FaIcon(
               FontAwesomeIcons.angleLeft,
@@ -42,7 +43,7 @@ class Myprescription extends StatelessWidget {
                 Container(
                   padding:const EdgeInsets.all(10),
                   decoration: BoxDecoration(
-                    color: Constants.primcolor,
+                    color: Constants.primcolor.withOpacity(0.7),
                     borderRadius: BorderRadius.circular(20)
                   ),
                   child:Row(
@@ -57,12 +58,58 @@ class Myprescription extends StatelessWidget {
             ),
           ),
 
-          ListView.builder(
-            itemCount: 4,
-            itemBuilder:(context, index) {
-              return Container();
+          Expanded(
+            child: ListView.builder(
+              itemCount: 4,
+              itemBuilder:(context, index) {
+                return Container(
+                  width: Get.width,
+                  height: 120,
+                  margin:const EdgeInsets.all(10),
+                  decoration:BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10)
+                  ),
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        width:80,
+                        child:
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text("12",style: TextStyle(fontSize: 20),),
+                            Text("2023")
+                          ],
+                        ),
+                      ),
+                      const SizedBox(width: 10,),
+                      
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children:const[
+                            SizedBox(height: 10,),
+                            Text("Prescription",style: TextStyle(
+                              fontWeight: FontWeight.bold,color: Constants.primcolor,
+                            ),),
+                            Text("for",style: TextStyle(
+                              color: Colors.black54,
+                            ),),
+                            Text("Amanuel demelash"),
+                             Text("male"),
+                          ],
+                        ),
+                      ),
+                      const FaIcon(FontAwesomeIcons.angleRight,color: Constants.primcolor,),
+                      const SizedBox(width: 15,)
+                    ],
+                  ),
+                );
 
-          },)
+            },),
+          )
         ],
       ),
     );
